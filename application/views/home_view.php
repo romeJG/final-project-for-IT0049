ba478916d7cf58a9c224315e18c9b0f847da7dfe
jastineee <201810083@fit.edu.ph>
2022-11-07 18:12:35 +0800

    Created new files - created header_view  >header_view has bootstrap - created navbar_view  > navbar_view has bootstrap navbar in it - Created home_view  > home_view has a controller

---
+++ application/views/home_view.php
@@ -0,0 +1,9 @@
+<!-- linking the css file -->
+
+<link rel="stylesheet" href="<?= base_url('assets/css/home.css'); ?>">
+
+<div class="body_container">
+
+    <h1 class="display-1 font-weight-bold">Welcome</h1>
+
+</div>
