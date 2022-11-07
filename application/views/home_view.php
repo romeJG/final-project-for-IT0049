dd251ed5c72d9d49a11872cc9ea35a5add438a65
jastineee <201810083@fit.edu.ph>
2022-11-07 19:17:20 +0800

    changed home to home_controller

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,6 +4,49 @@
 
 <div class="body_container">
 
-    <h1 class="display-1 font-weight-bold">Welcome</h1>
+    <h1 class="display-1 font-weight-bold">Welcome or logo</h1>
+
+    <div class="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit massa diam, vel tincidunt nisi dapibus vel. Aliquam tortor dolor, viverra semper laoreet pharetra, maximus id tellus. Fusce gravida, lectus sit amet porttitor convallis, augue quam varius ex, in aliquam tellus odio pretium elit. Curabitur id diam purus. Mauris non mattis velit. Morbi nec sem ac velit viverra luctus. Duis dolor mauris, mattis dapibus justo et, posuere tincidunt est. Sed velit mauris, dignissim at egestas sed, fringilla vel turpis.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit massa diam, vel tincidunt nisi dapibus vel. Aliquam tortor dolor, viverra semper laoreet pharetra, maximus id tellus. Fusce gravida, lectus sit amet porttitor convallis, augue quam varius ex, in aliquam tellus odio pretium elit. Curabitur id diam purus. Mauris non mattis velit. Morbi nec sem ac velit viverra luctus. Duis dolor mauris, mattis dapibus justo et, posuere tincidunt est. Sed velit mauris, dignissim at egestas sed, fringilla vel turpis.
+    </div>
+    <div class="row">
+        <div class="card text-white bg-dark mb-3 col-md item" style="width: 18rem;">
+            <img class="card-img-top" src="https://i.pinimg.com/originals/b8/72/95/b87295964f02e372deee5e2e91155fbf.png" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Item 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                <a href="#" class="btn btn-primary">Buy</a>
+                <a href="#" class="btn btn-primary">Add to cart</a>
+            </div>
+        </div>
+        <div class="card text-white bg-dark mb-3 col-md item" style="width: 18rem;">
+            <img class="card-img-top" src="https://i.pinimg.com/originals/b8/72/95/b87295964f02e372deee5e2e91155fbf.png" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Item 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                <a href="#" class="btn btn-primary">Buy</a>
+                <a href="#" class="btn btn-primary">Add to cart</a>
+            </div>
+        </div>
+        <div class="card text-white bg-dark mb-3 col-md item" style="width: 18rem;">
+            <img class="card-img-top" src="https://i.pinimg.com/originals/b8/72/95/b87295964f02e372deee5e2e91155fbf.png" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Item 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                <a href="#" class="btn btn-primary">Buy</a>
+                <a href="#" class="btn btn-primary">Add to cart</a>
+            </div>
+        </div>
+        <div class="card text-white bg-dark mb-3 col-md item" style="width: 18rem;">
+            <img class="card-img-top" src="https://i.pinimg.com/originals/b8/72/95/b87295964f02e372deee5e2e91155fbf.png" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Item 1</h5>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                <a href="#" class="btn btn-primary">Buy</a>
+                <a href="#" class="btn btn-primary">Add to cart</a>
+            </div>
+        </div>
+    </div>
 
 </div>
