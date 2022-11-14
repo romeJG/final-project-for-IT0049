9cf4cc08c6df63465bf6bb79c8619555e72bb968
jastineee <201810083@fit.edu.ph>
2022-11-14 18:33:11 +0800

    Added header

--- application/views/signup_view.php
+++ application/views/signup_view.php
@@ -3,5 +3,8 @@
 <link rel="stylesheet" href="<?= base_url('assets/css/signup.css'); ?>">
 
 <div class="body_container">
-
+    <div class="divider"><span></span><span>
+            Signup
+        </span><span></span>
+    </div>
 </div>
