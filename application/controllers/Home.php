7e38591f757ee7b1475b21695709500425915a96
jastineee <201810083@fit.edu.ph>
2022-11-26 14:20:54 +0800

    clean the code

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -9,7 +9,6 @@ class Home extends CI_Controller
         parent::__construct();
         $this->load->helper(array('form', 'url'));
         $this->load->library(array('form_validation', 'session'));
-
         $this->load->model('login_model');
     }
 
@@ -20,6 +19,10 @@ class Home extends CI_Controller
         $this->load->view('include/header_view', $data);
         $this->load->view('include/navbar_view', $data);
         $this->load->view('home_view');
+        $this->redirectIfSessExist();
+    }
+    public function redirectIfSessExist()
+    {
         //if no session it will not try to login.
         if ($this->session->userdata('email')) {
             //logs in the user if there is a session
