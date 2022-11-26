ab068f59cf6544b2f1f55b685e41cb443671528f
jastineee <201810083@fit.edu.ph>
2022-11-26 20:45:36 +0800

    updated home description

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -10,11 +10,14 @@
     </div>
     <div class="row logo-desc">
 
-        <img class="col-md-4" src="<?= base_url('assets/home/lukso_logo.png'); ?>" alt="Logo">
+        <img class="col-md-4 logo-img-desc" src="<?= base_url('assets/home/lukso_logo.png'); ?>" alt="Logo">
         <div class="col-md-1"></div>
         <div class="description col-md-7">
             <p>
-                <b class="display-3 font-weight-bold">H</b>ello World lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget risus vulputate, posuere mauris quis, efficitur eros. Morbi venenatis tempus metus, eu facilisis mauris ornare ac. Morbi ultricies, turpis vel aliquet commodo, urna mauris tincidunt nisl, ultricies finibus leo ipsum a libero. Vestibulum porttitor lorem tellus, sit amet gravida magna molestie nec. Morbi nec nisl mi. Aenean placerat eu justo in tincidunt. Proin convallis arcu dictum turpis blandit, sit amet fringilla nunc luctus. Maecenas tempus urna nec lacus iaculis, sit amet venenatis enim blandit.
+                <b class="display-3 font-weight-bold">H</b>ere ye, here ye! Ollivander's Wand Shop has just joined forces with the Lukso Team. The number of magicians in the globe is increasing, and with that comes the chance to have our wand selected for us online. If you'd want to save some floo powders, we can even have your wand delivered to your home by owl! Powered by Lukso, the Ollivanders Online Wand Shop welcomes your perusal.
+            </p>
+            <p>
+                Young witches and wizards who need to buy their first wand come here. Here's your chance to watch a wand choose a wizard in a one-of-a-kind, interactive show! Visitors (and the wizard they choose* in the interactive experience) may buy their very own wand, as well as wand sets, character wand reproductions, and more.
             </p>
         </div>
     </div>
