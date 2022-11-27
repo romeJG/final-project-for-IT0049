553c89f9dd48080844cf8be4d4046c92aac33eb5
jastineee <201810083@fit.edu.ph>
2022-11-27 16:17:39 +0800

    item details fix design

--- application/views/admin/admin_view_item_view.php
+++ application/views/admin/admin_view_item_view.php
@@ -3,7 +3,7 @@
 
     <link rel="stylesheet" href="<?= base_url('assets/css/admin_home.css'); ?>">
     <div class="row">
-        <div class="col-md-3"></div>
+        <div class="col-md-4"></div>
         <div class="col-md-5">
             <h3>Item Details</h3>
         </div>
@@ -26,7 +26,7 @@
                     <div class="row">
                         <div class="mb-3 col">
                             <label for="prod_name">Item Name</label>
-                            <input type="text" autocomplete="off" class="form-control" id="name" name="name" placeholder="Item Name" value="<?php echo $item->name; ?>">
+                            <input readonly type="text" autocomplete="off" class="form-control" id="name" name="name" placeholder="Item Name" value="<?php echo $item->name; ?>">
                         </div>
                         <div class="mb-3 col">
                             <label for="prod_name">Price</label>
@@ -34,7 +34,7 @@
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="inputGroup-sizing-default">€</span>
                                 </div>
-                                <input id="number" autocomplete="off" placeholder="Price" type="price" name="price" class="number form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" value="<?php echo $item->price; ?>">
+                                <input readonly id="number" autocomplete="off" placeholder="Price" type="price" name="price" class="number form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" value="<?php echo $item->price; ?>">
 
                             </div>
                         </div>
@@ -43,13 +43,13 @@
                     <div class="row">
                         <div class="mb-3 col">
                             <label for="prod_name">Short Description</label>
-                            <textarea type="text" rows="2" class=" form-control" id="short_description" name="short_description" placeholder="Shord Description"><?php echo $item->short_description; ?></textarea>
+                            <textarea readonly type="text" rows="2" class=" form-control" id="short_description" name="short_description" placeholder="Shord Description"><?php echo $item->short_description; ?></textarea>
                         </div>
                     </div>
                     <div class="row">
                         <div class="mb-3 col">
                             <label for="prod_name">Long Description</label>
-                            <textarea type="text" rows="5" class="form-control" id="description" name="description" placeholder="Long Description"><?php echo $item->description; ?></textarea>
+                            <textarea readonly type="text" rows="5" class="form-control" id="description" name="description" placeholder="Long Description"><?php echo $item->description; ?></textarea>
                         </div>
                     </div>
                 </div>
@@ -59,7 +59,7 @@
                 <div class="row col">
                     <div class="col">
                         <label for="prod_name">Date created</label>
-                        <input type="text" autocomplete="off" class="form-control" id="name" name="name" placeholder="Item Name" value="<?php echo $item->date_created; ?>">
+                        <input readonly type="text" autocomplete="off" class="form-control" id="name" name="name" placeholder="Item Name" value="<?php echo $item->date_created; ?>">
                     </div>
                     <?php if ($item->is_active) {
                         $status = "Active";
@@ -69,7 +69,7 @@
                     ?>
                     <div class="col">
                         <label for="prod_name">Is active</label>
-                        <input type="text" autocomplete="off" class="form-control" id="name" name="name" placeholder="Item Name" value="<?php echo $status; ?>">
+                        <input readonly type="text" autocomplete="off" class="form-control" id="name" name="name" placeholder="Item Name" value="<?php echo $status; ?>">
                     </div>
                 </div>
 
